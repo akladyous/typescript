42b081ac73e1ad40c02cba4bc52a6645b04f0e2d
Boula Akladyous <akladyous@gmail.com>
2023-04-06 14:44:50 -0400

    Untracked files on typescript

--- function.ts
+++ function.ts
@@ -1,58 +1,58 @@
 function add(x: number, y: number): number {
-  return x + y;
+    return x + y;
 }
 let xPlusY = add(1, 1);
 
 function voidFunction(): void {
-  console.log('void');
+    console.log('void');
 }
 // function signature //
 type Signature1 = (num1: number, num2: number) => number;
 const addNumbers1: Signature1 = function (num1: number, num2: number): number {
-  return num1 + num2;
+    return num1 + num2;
 };
 // ----------------------------------------------------------------------------------
 type Signature2 = {
-  (x: number, y: number): number;
+    (x: number, y: number): number;
 };
 const addTwoNumbers1: Signature2 = function (x: number, y: number): number {
-  return x + y;
+    return x + y;
 };
 // ----------------------------------------------------------------------------------
 type Signature3 = {
-  (num1: number, num2: number): number;
-  defaultNum2: number;
+    (num1: number, num2: number): number;
+    defaultNum2: number;
 };
 const addNumbers2: Signature3 = (num1, num2 = addNumbers2.defaultNum2) => {
-  return num1 + num2;
+    return num1 + num2;
 };
 addNumbers2.defaultNum2 = 0; // set the default value for num2
 // ----------------------------------------------------------------------------------
 //Generic Functions
 function firstElement<Type>(arr: Type[]): Type | undefined {
-  return arr[0];
+    return arr[0];
 }
 firstElement(['a', 'b', 'c']);
 firstElement([true, false, true]);
 // ----------------------------------------------------------------------------------
 function map<Input, Output>(arr: Input[], func: (arg: Input) => Output): Output[] {
-  return arr.map(func);
+    return arr.map(func);
 }
 const parsed = map(['1', '2', '3'], (n) => parseInt(n));
 // ----------------------------------------------------------------------------------
 function argsFunc1(numbers: number[]) {
-  for (const num of numbers) {
-    console.log(num);
-  }
+    for (const num of numbers) {
+        console.log(num);
+    }
 }
 argsFunc1([1, 2, 3, 4]);
 // ----------------------------------------------------------------------------------
 function longest<Type extends { length: number }>(a: Type, b: Type) {
-  debugger;
-  if (a.length >= b.length) {
-    return a;
-  } else {
-    return b;
-  }
+    debugger;
+    if (a.length >= b.length) {
+        return a;
+    } else {
+        return b;
+    }
 }
 longest('a', 'a');
---
+++ types.ts
@@ -0,0 +1,12 @@
+let num1: Number;
+let t1: [number, number];
+t1 = [1, 2];
+
+enum Colors {
+    Red = 1,
+    Green = 2,
+    Blue = 3,
+}
+let color = Colors.Blue;
+color = Colors.Green;
+console.log(Colors[3]);
