7852dd11a3ddff6302b0e3dc9885244c1d72dfe8
Boula Akladyous <akladyous@gmail.com>
2023-04-26 09:38:35 -0400

    Untracked files on typescript

--- prettier.config.js
+++ prettier.config.js
@@ -1,20 +1,5 @@
-module.exports = {
-  trailingCommma: 'all',
-  arrowParens: 'always',
-  tabWidth: 4,
-  semi: true,
-  singleQuote: true,
-  printWidth: 100,
-  // overrides: [
-  //   {
-  //     files: ['*.js', '*.jsx', '*.html'],
-  //     options: {
-  //       semi: true,
-  //       tabWidth: 2,
-  //     },
-  //   },
-  // ],
-  plugins: [
-    // require("prettier-plugin-tailwindcss")
-  ],
-};
+{
+  "trailingComma": "es5",
+  "semi": false,
+  "singleQuote": true
+}
