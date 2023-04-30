e049315c9db20ab42dba0f91596cf13d6956d8e9
Boula Akladyous <akladyous@gmail.com>
2023-04-30 11:01:14 -0400

    Untracked files on typescript

--- src/functions/Function Type Aliases.ts
+++ src/functions/Function Type Aliases.ts
@@ -2,30 +2,19 @@ type StringToNumber = (input: string) => number;
 let stringToNumber: StringToNumber;
 stringToNumber = (input) => input.length; // Ok
 
-
-
-function sum(a: number, b: number, c?: number) : number {
-  if (c) {
-    return a+b+c
-  }
-  return a+ b
-}
-
-
-
-
-function add(a: number): any;
-function add(a: number, b: number): any;
-function add(a: string, b: string): any;
+function add(a: number): number;
+function add(a: number, b: number): number;
 function add(a: number, b: number, c: number): number;
-function add(a: any, b: any, c?: any): any {
-  if (c) {
-    return a + c;
-  }
-  if (typeof a === 'string') {
-    return `a is ${a}, b is ${b}`;
-  } else {
-    return a + b;
+function add(a: number, b?: number, c?:number): number {
+  if (b === undefined && c === undefined) { // if only one parameter is passed
+    return a;
+  } else { // if two or three parameters are passed
+    return (a + (b ?? 0) + (c ?? 0));
   }
 }
 
+
+
+// var a0 = undefined, b0=undefined, c0=undefined
+// var a1 = 1, b1=1, c1 = 1
+// How to verify if three numbers are equal using logical operators
