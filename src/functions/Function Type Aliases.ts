a27768ac2ef668d3b6e94b35fc70ef203b19a1fb
Boula Akladyous <akladyous@gmail.com>
2023-04-30 10:26:08 -0400

    Untracked files on typescript

--- src/functions/Function Type Aliases.ts
+++ src/functions/Function Type Aliases.ts
@@ -3,3 +3,29 @@ let stringToNumber: StringToNumber;
 stringToNumber = (input) => input.length; // Ok
 
 
+
+function sum(a: number, b: number, c?: number) : number {
+  if (c) {
+    return a+b+c
+  }
+  return a+ b
+}
+
+
+
+
+function add(a: number): any;
+function add(a: number, b: number): any;
+function add(a: string, b: string): any;
+function add(a: number, b: number, c: number): number;
+function add(a: any, b: any, c?: any): any {
+  if (c) {
+    return a + c;
+  }
+  if (typeof a === 'string') {
+    return `a is ${a}, b is ${b}`;
+  } else {
+    return a + b;
+  }
+}
+
