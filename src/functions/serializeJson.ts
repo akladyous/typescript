c21b060b932a08b52b96a9dfcbf6a120016c1de5
Boula Akladyous <akladyous@gmail.com>
2023-07-27 23:07:29 -0400

    Untracked files on typescript

--- src/functions/serializeJson.ts
+++
@@ -1,9 +0,0 @@
-
-// ----------------------------------------------------------------------------------
-function serializeJson(value: any, options?: SerializationOption) {
-    const indentation = options?.format?.indend;
-    return JSON.stringify(value, null, indentation);
-}
-const u1 = { name: 'paolo' };
-serializeJson(u1);
-serializeJson(u1, { format: { indend: 2 } });
--- util/util.ts
+++
@@ -1,7 +0,0 @@
-export const setProperty = (property: string, value: string): void => {
-  document.documentElement.style.setProperty(property, value);
-};
-
-export const removeProperty = (property: string): string => {
-  return document.documentElement.style.removeProperty(property);
-};
